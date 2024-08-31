a7ceec817df78b9511993fbe480597cbd1ca84ec
asimov <seyranmustafa4@gmail.com>
2024-09-01 00:36:27 +0300

    p

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./components/Header";
+import Questions from "./components/Questions";
 
 
 function App() {
   return (
     <div>
       <Header/>
+      <Questions/>
     </div>
   );
 }
---
+++ src/components/Questions.jsx
@@ -0,0 +1,12 @@
+import data from "../helpers/data";
+import icons from "../helpers/icons";
+
+
+const Questions = () => {
+  console.log(data);
+  return (
+    <div>Questions</div>
+  )
+}
+
+export default Questions
