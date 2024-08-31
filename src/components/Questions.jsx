d1183621ea2ac26599f7eae74aadb8da11f99dce
asimov <seyranmustafa4@gmail.com>
2024-09-01 01:05:40 +0300

    p

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,12 +1,29 @@
-import data from "../helpers/data";
-import icons from "../helpers/icons";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import questions from "../helpers/data";
+import {arrowdown} from "../helpers/icons";
+import {arrowup} from "../helpers/icons";
 
 
 const Questions = () => {
-  console.log(data);
+  // console.log(questions);
+  console.log(arrowdown);
   return (
-    <div>Questions</div>
-  )
-}
+    <Container>
+      <Row>
+        {questions.map((item) => (
+          <>
+            <Col sm={8}>{item.question}</Col>
+            <Col sm={4}>{arrowdown}</Col>
+          </>
+        ))}
+      
 
-export default Questions
+        {/* <Col sm={8}>{item.answer}</Col> */}
+      </Row>
+    </Container>
+  );
+};
+
+export default Questions;
